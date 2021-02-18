832ba9a350733811000011e5fc787e13fb98f5f3
ftbyu <ftb.y.m.kashima.07@gmail.com>
2021-02-18 18:30:19 +0900

    [Fix] 環境差異の吸収

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const stripe = require("stripe")("sk_test_xxx");
+const env = require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const logger = require("../logger");
 
 /* GET home page. */
@@ -8,104 +9,104 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
 router.post("/v1/order/payment", async function(req, res, next){
 
-    logger.info("ルータメソッドの処理を開始します. リクエスト : ", req.body);
+  logger.info("ルータメソッドの処理を開始します. リクエスト : ", req.body);
 
-    const { paymentMethodId, paymentIntentId, items, currency, useStripeSdk } = req.body;
+  const { paymentMethodId, paymentIntentId, items, currency, useStripeSdk } = req.body;
 
-    const total = calculateAmount(req.body.items);
+  const total = calculateAmount(items);
 
-    try { 
-        let intent;
-        if (paymentMethodId) {
-            const request = {
-                amount: total,
-                currency: currency,
-                payment_method: paymentMethodId,
-                confirmation_method: "manual",
-                confirm: true,
-                use_stripe_sdk: useStripeSdk,
-            }
+  try {
+    let intent;
+    if (req.body.paymentMethodId) {
+      const request = {
+        amount: total,
+        currency: currency,
+        payment_method: paymentMethodId,
+        confirmation_method: "manual",
+        confirm: true,
+        use_stripe_sdk: useStripeSdk,
+      }
 
-            logger.info("Stripe APIを呼び出します. リクエスト : ", request);
-            intent = await stripe.paymentIntents.create(request);
-            logger.info("Stripe APIを呼び出しました. レスポンス : ", intent);
-
-        } else if (paymentIntentId) {
-            intent = await stripe.paymentIntents.confirm(paymentIntentId);
-        }
-
-        const response = generateResponse(intent);
-
-        logger.info("ルータメソッドの処理を終了します. レスポンス : ", response);
-        
-        res.send(response);
-    } catch (e) {
-        logger.error("ルータメソッドの処理中にエラーが発生しました : ", e);
-        const response = generateErrorResponse(e.message);
-
-        res.status(500);
-        res.send(response);
+      logger.info("Stripe APIを呼び出します. リクエスト : ", request);
+      intent = await stripe.paymentIntents.create(request);
+      logger.info("Stripe APIを呼び出しました. レスポンス : ", intent);
+    } else if (req.body.paymentIntentId) {
+      intent = await stripe.paymentIntents.confirm(req.body.paymentIntentId);
     }
+
+    const response = generateResponse(intent);
+
+    logger.info("ルータメソッドの処理を終了します. レスポンス : ", response);
+    res.send(response);
+  } catch (e) {
+    logger.error("ルータメソッドの処理中にエラーが発生しました : ", e);
+    const response = generateErrorResponse(e.message);
+
+    res.status(500);
+    res.send(response);
+  }
 });
 
 function calculateAmount(items) {
-    let total = 0;
-    for (let i = 0; i < items.length; i++) {
-        const current = items[i].amount * items[i].quantity;
-        total += current;
-    }
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    const current = items[i].amount * items[i].quantity;
+    total += current;
+  }
 
-    return total;
+  return total;
 }
 
 function generateResponse(paymentIntent) {
-    let response = {
-        requiresAction: false,
-        clientSecret: "",
-        paymentIntentStatus : ""
-    }
+  let response = {
+    requiresAction: false,
+    clientSecret: "",
+    paymentIntentStatus : ""
+  }
 
-    switch (paymentIntent.status) {
-        case "requires_action":
-            response.paymentIntentStatus = "requires_action";
-            break;
-        case "requires_source_action":
-            response.paymentIntentStatus = "requires_source_action";
-            response.requiresAction = true;
-            response.clientSecret = paymentIntent.client_secret;
-            break;
-        case "requires_payment_method":
-            response.paymentIntentStatus = "requires_payment_method";
-            break;
-        case "requires_source":
-            response.paymentIntentStatus = "requires_source";
-            response.error = {
-                messages : ["カードが拒否されました。別の決済手段をお試しください"]
-            }
-            break;
-        case "succeeded":
-            response.paymentIntentStatus = "succeeded";
-            response.clientSecret = paymentIntent.client_secret;
-            break;
-        default:
-            response.error = {
-                messages : ["システムエラーが発生しました"]
-            }
-            break;
-    }
+  switch (paymentIntent.status) {
+    case "requires_action":
+      response.paymentIntentStatus = "requires_action";
+      break;
+    case "requires_source_action":
+      response.paymentIntentStatus = "requires_source_action";
+      response.requiresAction = true;
+      response.clientSecret = paymentIntent.client_secret;
+      break;
+    case "requires_payment_method":
+      response.paymentIntentStatus = "requires_payment_method";
+      break;
+    case "requires_source":
+      response.paymentIntentStatus = "requires_source";
+      response.error = {
+          messages : ["カードが拒否されました。別の決済手段をお試しください"]
+      }
+      break;
+    case "succeeded":
+      response.paymentIntentStatus = "succeeded";
+      response.clientSecret = paymentIntent.client_secret;
+      break;
+    default:
+      response.error = {
+          messages : ["システムエラーが発生しました"]
+      }
+      break;
+  }
 
-    return response;
+  return response;
 }
 
 function generateErrorResponse (error) {
 
-    return {
-        error : {
-        messages : [error]
-        }
+  return {
+    error : {
+      messages : [error]
     }
+  }
+
 }
 
 module.exports = router;
